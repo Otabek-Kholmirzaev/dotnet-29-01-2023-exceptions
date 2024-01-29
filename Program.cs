b57c8bf2f35660a2cb9b8948150047a343fbee43
Otabek Kholmirzaev <otabekmirzo@outlook.com>
2024-01-29 10:49:52 +0500

    initial commit.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+AppDomain appDomain = AppDomain.CurrentDomain;
+
+appDomain.UnhandledException += new UnhandledExceptionEventHandler(HandleException);
+int num = int.Parse(ReadLine()!);
+
+void HandleException(object sender, UnhandledExceptionEventArgs e)
+{
+    WriteLine($"Sorry there was a problem and we need to close. Details: {e.ExceptionObject}");
+}
